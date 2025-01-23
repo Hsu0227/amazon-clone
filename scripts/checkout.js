f63046a811bb61c1b5374d0ff5de5b11a18f3b2b
Chenghao Hsu <chenghao@broadside.digital>
2025-01-22 16:55:33 -0800

    delete product from HTML when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ carts.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -95,6 +95,11 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const { productId } = link.dataset;
     removeFromCart(productId);
-    console.log(carts);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+
+    container.remove();
   });
 });
